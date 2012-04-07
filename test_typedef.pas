3639bc24d4296ad7897ea7758399d33c204d78e9
yl790 <yl790@nyu.edu>
2012-04-07 00:02:43 -0400

    further testing

--- test_typedef.pas
+++ test_typedef.pas
@@ -1,17 +1,37 @@
 program errorfree;
 type
+	i = integer;
+	int = i;
+	j = i;
+	k = int;
+	l = int;
+	h = l;
 	s = string;
+	rs = s;
 	r = record
 		a,b : integer;
 		c   : s
 	end;
 	y = array[1..10] of r;
-	
+	R = record
+		u,v : l;
+		str : string
+	end;
+	S = record
+		uu, vv : i;
+		str1 : string;
+		str2 : rs
+	end;
 var
 	z : s;
+	rec : r;
+	REc : R;
+	SS : S;
 
 function foo1(a : integer) : r; 
 begin
+   REc := rec;
+   SS := rec;
    a := 0
 end;
 
